c5c68cc69fce9c1eb77db8f0975f921e3a6b29c5
Kristin <kristindyulgeryan@abv.bg>
2025-04-29 13:39:31 +0300

    Add footer component.

--- my-app/src/components/Footer.tsx
+++ my-app/src/components/Footer.tsx
@@ -1,5 +1,101 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="py-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 bg-gray-100 text-sm mt-24">
+      {/* Top */}
+      <div className="flex flex-col md:flex-row justify-between gap-24">
+        {/* Left */}
+        <div className="w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
+          <Link href="/">
+            <div className="text-2xl tracking-wide">Avediss Gold</div>
+          </Link>
+          <p>36 Mutkurova, Ruse, Bulgaria</p>
+          <span className="font-semibold">avediss@gold.com</span>
+          <span className="font-semibold">+359 888 309393</span>
+          <div className="flex gap-6">
+            <Image src="/facebook.png" alt="" width={16} height={16} />
+            <Image src="/instagram.png" alt="" width={16} height={16} />
+            <Image src="/youtube.png" alt="" width={16} height={16} />
+          </div>
+        </div>
+        {/* Center */}
+        <div className="hidden lg:flex justify-between w-1/2">
+          <div className="flex flex-col justify-between">
+            <h1 className="font-medium text-lg">COMPANY</h1>
+            <div className="flex flex-col gap-6 ">
+              <Link href="/">About Us</Link>
+              <Link href="/">Contact Us</Link>
+              <Link href="/">Blog</Link>
+              <Link href="/">Shipping Policy</Link>
+              <Link href="/">Refund Policy</Link>
+            </div>
+          </div>
+          <div className="flex flex-col justify-between">
+            <h1 className="font-medium text-lg">SHOP</h1>
+            <div className="flex flex-col gap-6 ">
+              <Link href="/">New Arrivals</Link>
+              <Link href="/">kids</Link>
+              <Link href="/">Men</Link>
+              <Link href="/">Women</Link>
+              <Link href="/">All Products</Link>
+            </div>
+          </div>
+          <div className="flex flex-col justify-between">
+            <h1 className="font-medium text-lg">HELP</h1>
+            <div className="flex flex-col gap-6 ">
+              <Link href="/">Customer Service</Link>
+              <Link href="/">My Account</Link>
+              <Link href="/">Find an Atelier</Link>
+              <Link href="/">Legal and Privacy</Link>
+              <Link href="/">Gift Card</Link>
+            </div>
+          </div>
+        </div>
+
+        {/* Right */}
+        <div className="w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
+          <h1 className="font-medium text-lg">SUBSCRIBE</h1>
+          <p>
+            Be the first to get the latest news about trends, promotions, and
+            mush more!
+          </p>
+          <div className="flex">
+            <input
+              type="text"
+              placeholder="Email address"
+              className="p-4 w-3/4 bg-white text-black"
+            />
+            <button className="w-1/4 bg-pink-500 text-white">JOIN</button>
+          </div>
+          <span className="font-semibold">Secure Payments</span>
+          <div className="flex justify-between">
+            <Image src="/visa.png" alt="" width={40} height={20} />
+            <Image src="/mastercard.png" alt="" width={40} height={20} />
+            <Image src="/paypal.png" alt="" width={40} height={20} />
+            <Image src="/discover.png" alt="" width={40} height={20} />
+          </div>
+        </div>
+      </div>
+      {/* Bottom */}
+      <div className="flex flex-col md:flex-row items-center justify-between gap-8 mt-16">
+        <div className="">@ 2025 Avediss Gold</div>
+        <div className="flex flex-col gap-8 md:flex-row">
+          <div className="">
+            <span className="text-gray-500 mr-4">Language</span>
+            <span className="font-medium">Bulgaria | English </span>
+          </div>
+        </div>
+        <div className="">
+          <div className="">
+            <span className="text-gray-500 mr-4">Currency</span>
+            <span className="font-medium">€ EURO</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
