7f0a1dc4777d8084e6a5e4a06b8ef64b2d68bf99
Kristin <kristindyulgeryan@abv.bg>
2025-04-29 17:27:59 +0300

    Add product images component.

---
+++ my-app/src/components/ProductImages.tsx
@@ -0,0 +1,52 @@
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
+
+const images = [
+  {
+    id: 1,
+    url: "https://images.pexels.com/photos/11310658/pexels-photo-11310658.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+  },
+  {
+    id: 2,
+    url: "https://images.pexels.com/photos/204427/pexels-photo-204427.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+];
+
+const ProductImages = () => {
+  const [index, setIndex] = useState(0);
+  return (
+    <div className="">
+      <div className="h-[500px] relative">
+        <div className="">
+          <Image
+            src={images[index].url}
+            alt=""
+            fill
+            sizes="30vw"
+            className="object-cover rounded-md"
+          />
+        </div>
+      </div>
+      <div className="flex justify-between gap-4 mt-8">
+        {images.map((img, i) => (
+          <div
+            className="w-1/2 h-32 relative gap-4 mt-8"
+            key={img.id}
+            onClick={() => setIndex(i)}
+          >
+            <Image
+              src={img.url}
+              alt=""
+              fill
+              sizes="30vw"
+              className="object-cover rounded-md"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductImages;
