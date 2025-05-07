219fbed95e5fe2c21919235024190dfeb1c78685
Kristin <kristindyulgeryan@abv.bg>
2025-05-07 22:23:29 +0300

    Implement single product page fetching from Wix
    Update product list page.

--- my-app/src/components/ProductImages.tsx
+++ my-app/src/components/ProductImages.tsx
@@ -2,25 +2,25 @@
 import Image from "next/image";
 import React, { useState } from "react";
 
-const images = [
-  {
-    id: 1,
-    url: "https://images.pexels.com/photos/11310658/pexels-photo-11310658.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-  },
-  {
-    id: 2,
-    url: "https://images.pexels.com/photos/204427/pexels-photo-204427.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  },
-];
+// const images = [
+//   {
+//     id: 1,
+//     url: "https://images.pexels.com/photos/11310658/pexels-photo-11310658.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+//   },
+//   {
+//     id: 2,
+//     url: "https://images.pexels.com/photos/204427/pexels-photo-204427.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//   },
+// ];
 
-const ProductImages = () => {
+const ProductImages = ({ items }: { items: any }) => {
   const [index, setIndex] = useState(0);
   return (
     <div className="">
       <div className="h-[500px] relative">
         <div className="">
           <Image
-            src={images[index].url}
+            src={items[index].image?.url}
             alt=""
             fill
             sizes="30vw"
@@ -29,14 +29,14 @@ const ProductImages = () => {
         </div>
       </div>
       <div className="flex justify-between gap-4 mt-8">
-        {images.map((img, i) => (
+        {items.map((item: any, i: number) => (
           <div
             className="w-1/2 h-32 relative gap-4 mt-8 cursor-pointer"
-            key={img.id}
+            key={item._id}
             onClick={() => setIndex(i)}
           >
             <Image
-              src={img.url}
+              src={item.image?.url}
               alt=""
               fill
               sizes="30vw"
