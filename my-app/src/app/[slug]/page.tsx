60b7b3f60ecf6b7bbb160645fe1048285384f087
Kristin <kristindyulgeryan@abv.bg>
2025-05-08 17:12:39 +0300

    Implement product option selection with stock check and UI indicators for selected/disabled states.

--- my-app/src/app/[slug]/page.tsx
+++ my-app/src/app/[slug]/page.tsx
@@ -18,7 +18,7 @@ const SinglPage = async ({ params }: { params: { slug: string } }) => {
   }
 
   const product = products.items[0];
-  console.log(product);
+  console.log(product.productOptions);
 
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
@@ -44,7 +44,13 @@ const SinglPage = async ({ params }: { params: { slug: string } }) => {
           </div>
         )}
         <div className="h-[2px] bg-gray-100" />
-        <CustomizeProducts />
+        {product._id && product.variants && product.productOptions && (
+          <CustomizeProducts
+            productId={product._id}
+            variants={product.variants}
+            productOptions={product.productOptions}
+          />
+        )}
         <Add />
         <div className="h-[2px] bg-gray-100" />
         {product.additionalInfoSections?.map((section: any) => (
--- my-app/src/components/CustomizeProducts.tsx
+++ my-app/src/components/CustomizeProducts.tsx
@@ -1,20 +1,88 @@
-const CustomizeProducts = () => {
+"use client";
+import { products } from "@wix/stores";
+import { useState } from "react";
+
+const CustomizeProducts = ({
+  productId,
+  variants,
+  productOptions,
+}: {
+  productId: string;
+  variants: products.Variant[];
+  productOptions: products.ProductOption[];
+}) => {
+  const [selectedOptions, setSelectedOptions] = useState<{
+    [key: string]: string;
+  }>({});
+
+  const hanldeOptionSelect = (optionType: string, choice: string) => {
+    setSelectedOptions((prev) => ({ ...prev, [optionType]: choice }));
+  };
+
+  const isVariantInStock = (choices: { [key: string]: string }) => {
+    return variants.some((variant) => {
+      const variantChoices = variant.choices;
+      if (!variantChoices) return false;
+      // Check if all selected options match the variant's choices
+      return (
+        Object.entries(choices).every(
+          ([key, value]) => variantChoices[key] === value
+        ) &&
+        variant.stock?.inStock &&
+        variant.stock?.quantity &&
+        variant.stock?.quantity > 0
+      );
+    });
+  };
+
+  console.log(variants);
+
   return (
     <div className="flex flex-col gap-6">
-      <h4 className="font-medium">Choose a color</h4>
-      <ul className="flex items-center gap-3">
-        <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-pointer relative bg-yellow-500">
-          <div className="absolute w-10 h-10 rounded-full ring-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
-        </li>
-        <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-pointer relative bg-gray-400"></li>
-        <li
-          className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-not-allowed relative"
-          style={{ backgroundColor: "#ECC5C0" }}
-        >
-          <div className="absolute w-10 h-[2px] bg-red-400 rotate-45  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
-        </li>
-      </ul>
-      <h4 className="font-medium">Choose a karat</h4>
+      {productOptions.map((optionType) => (
+        <div className="flex flex-col gap-4" key={optionType.name}>
+          <h4 className="font-medium">{optionType.name}</h4>
+          {optionType.choices?.map((choice) => {
+            const disabled = !isVariantInStock({
+              ...selectedOptions,
+              [optionType.name!]: choice.description!,
+            });
+            const selected =
+              selectedOptions[optionType.name!] === choice.description!;
+
+            return (
+              <div
+                className=""
+                key={choice.value}
+                onClick={() =>
+                  hanldeOptionSelect(optionType.name!, choice.description!)
+                }
+              >
+                {choice.description}
+                {disabled && "disabled"}
+                {selected && "selected"}
+              </div>
+            );
+          })}
+        </div>
+      ))}
+
+      {/* Color */}
+      {/*
+          <ul className="flex items-center gap-3">
+            <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-pointer relative bg-yellow-500">
+              <div className="absolute w-10 h-10 rounded-full ring-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+            </li>
+            <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-pointer relative bg-gray-400"></li>
+            <li
+              className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-not-allowed relative"
+              style={{ backgroundColor: "#ECC5C0" }}
+            >
+              <div className="absolute w-10 h-[2px] bg-red-400 rotate-45  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+            </li>
+          </ul> */}
+      {/* Others */}
+      {/* <h4 className="font-medium">Choose a karat</h4>
       <ul className="flex items-center gap-3">
         <li className="ring-1 ring-pink-500 text-white  bg-pink-500 rounded-md py-1 px-4 text-sm cursor-pointer ">
           14K
@@ -25,7 +93,7 @@ const CustomizeProducts = () => {
         <li className="ring-1 ring-pink-200 text-white bg-pink-200 rounded-md py-1 px-4 text-sm cursor-not-allowed ">
           24K
         </li>
-      </ul>
+      </ul> */}
     </div>
   );
 };
