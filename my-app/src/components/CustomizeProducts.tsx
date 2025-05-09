e5bf5fb11a0d046453c3f385ea7cef7289b125e1
Kristin <kristindyulgeryan@abv.bg>
2025-05-09 14:01:28 +0300

    Update customize products.

--- my-app/src/components/CustomizeProducts.tsx
+++ my-app/src/components/CustomizeProducts.tsx
@@ -15,15 +15,17 @@ const CustomizeProducts = ({
     [key: string]: string;
   }>({});
 
-  const hanldeOptionSelect = (optionType: string, choice: string) => {
+  const handleOptionSelect = (optionType: string, choice: string) => {
     setSelectedOptions((prev) => ({ ...prev, [optionType]: choice }));
   };
 
   const isVariantInStock = (choices: { [key: string]: string }) => {
+    console.log("Checking choices:", choices);
     return variants.some((variant) => {
       const variantChoices = variant.choices;
+
       if (!variantChoices) return false;
-      // Check if all selected options match the variant's choices
+
       return (
         Object.entries(choices).every(
           ([key, value]) => variantChoices[key] === value
@@ -35,35 +37,65 @@ const CustomizeProducts = ({
     });
   };
 
-  console.log(variants);
-
   return (
     <div className="flex flex-col gap-6">
       {productOptions.map((optionType) => (
         <div className="flex flex-col gap-4" key={optionType.name}>
           <h4 className="font-medium">{optionType.name}</h4>
-          {optionType.choices?.map((choice) => {
-            const disabled = !isVariantInStock({
-              ...selectedOptions,
-              [optionType.name!]: choice.description!,
-            });
-            const selected =
-              selectedOptions[optionType.name!] === choice.description!;
+          <ul className="flex items-center gap-3">
+            {optionType.choices?.map((choice) => {
+              const disabled = !isVariantInStock({
+                ...selectedOptions,
+                [optionType.name!]: choice.description!,
+              });
 
-            return (
-              <div
-                className=""
-                key={choice.value}
-                onClick={() =>
-                  hanldeOptionSelect(optionType.name!, choice.description!)
-                }
-              >
-                {choice.description}
-                {disabled && "disabled"}
-                {selected && "selected"}
-              </div>
-            );
-          })}
+              const selected =
+                selectedOptions[optionType.name!] ===
+                choice.description?.toLowerCase();
+
+              const clickHandler = disabled
+                ? undefined
+                : () =>
+                    handleOptionSelect(optionType.name!, choice.description!);
+
+              return optionType.name === "Color" ? (
+                <li
+                  key={choice.description}
+                  className="w-8 h-8 rounded-full ring-1 ring-gray-300  relative "
+                  style={{
+                    backgroundColor: choice.value,
+                    cursor: disabled ? "not-allowed" : "pointer",
+                  }}
+                  onClick={clickHandler}
+                >
+                  {selected && (
+                    <div className="absolute w-10 h-10 rounded-full ring-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+                  )}
+                  {disabled && (
+                    <div className="absolute w-10 h-[2px] bg-red-400 rotate-45  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+                  )}
+                </li>
+              ) : (
+                <li
+                  className="ring-1 ring-pink-500 text-ring-pink-500  rounded-md py-1 px-4 text-sm"
+                  style={{
+                    cursor: disabled ? "not-allowed" : "pointer",
+                    backgroundColor: selected
+                      ? "#f35c7a"
+                      : disabled
+                      ? "#FBCFE8"
+                      : "white",
+                    color: selected || disabled ? "white" : "#f35c7a",
+                    boxShadow: disabled ? "none" : "",
+                  }}
+                  onClick={clickHandler}
+                  key={choice.description}
+                >
+                  {choice.description}
+                </li>
+              );
+            })}
+          </ul>
         </div>
       ))}
 
@@ -99,3 +131,16 @@ const CustomizeProducts = ({
 };
 
 export default CustomizeProducts;
+
+// <div
+// className=""
+// key={choice.value}
+// onClick={() =>
+//   hanldeOptionSelect(optionType.name!, choice.description!)
+// }
+// >
+// {choice.description}
+// {disabled && "disabled"}
+// {selected && "selected"}
+// </div>
+// );
