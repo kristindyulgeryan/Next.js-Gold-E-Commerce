5822870b926e868777cf97569d2fbdb327ca9dd6
Kristin <kristindyulgeryan@abv.bg>
2025-05-15 10:54:28 +0300

    Install @wix/members.

---
+++ my-app/src/app/profile/page.tsx
@@ -0,0 +1,13 @@
+import { wixClientServer } from "@/lib/wixClientServer";
+
+const ProfilePage = async () => {
+  const wixClient = await wixClientServer();
+  return (
+    <div className="flex flex-col md:flex-row gap-24 md:h-[calc(100vh-180px)] items-center px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
+      <div className="w-full md:w-1/2">User</div>
+      <div className="w-full md:w-1/2">Orders</div>
+    </div>
+  );
+};
+
+export default ProfilePage;
