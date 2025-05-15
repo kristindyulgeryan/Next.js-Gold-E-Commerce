9533f93baad36bd83388b8e297863c365aa18b81
Kristin <kristindyulgeryan@abv.bg>
2025-05-15 14:10:21 +0300

    Add server action to update Wix member data from form

--- my-app/src/app/profile/page.tsx
+++ my-app/src/app/profile/page.tsx
@@ -1,4 +1,5 @@
 import UpdateButton from "@/components/UpdateButton";
+import { updateUser } from "@/lib/actions";
 import { wixClientServer } from "@/lib/wixClientServer";
 import { members } from "@wix/members";
 import Link from "next/link";
@@ -14,7 +15,7 @@ const ProfilePage = async () => {
   console.log(user);
 
   if (!user.member?.contactId) {
-    <div>Not logged in!</div>;
+    return <div>Not logged in!</div>;
   }
   const orderRes = await wixClient.orders.searchOrders({
     search: {
@@ -27,7 +28,8 @@ const ProfilePage = async () => {
     <div className="flex flex-col md:flex-row gap-24 md:h-[calc(100vh-180px)] items-center px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
       <div className="w-full md:w-1/2">
         <h1 className="text-2xl">Profile</h1>
-        <form className="mt-12 flex flex-col gap-4">
+        <form action={updateUser} className="mt-12 flex flex-col gap-4">
+          <input type="text" hidden name="id" value={user.member?.contactId} />
           <label className="text-sm text-gray-700">Username</label>
           <input
             type="text"
@@ -35,21 +37,21 @@ const ProfilePage = async () => {
             placeholder={user.member?.profile?.nickname || "John"}
             className="ring-1 ring-gray-300 rounded-md p-2 max-w-96"
           />
-          <label className="text-sm text-gray-700">Username</label>
+          <label className="text-sm text-gray-700">First Name</label>
           <input
             type="text"
             name="firstname"
             placeholder={user.member?.contact?.firstName || "John"}
             className="ring-1 ring-gray-300 rounded-md p-2 max-w-96"
           />
-          <label className="text-sm text-gray-700">Username</label>
+          <label className="text-sm text-gray-700">Surname</label>
           <input
             type="text"
             name="lastname"
             placeholder={user.member?.contact?.lastName || "Doe"}
             className="ring-1 ring-gray-300 rounded-md p-2 max-w-96"
           />
-          <label className="text-sm text-gray-700">Username</label>
+          <label className="text-sm text-gray-700">Phone</label>
           <input
             type="text"
             name="phone"
@@ -60,7 +62,7 @@ const ProfilePage = async () => {
             }
             className="ring-1 ring-gray-300 rounded-md p-2 max-w-96"
           />
-          <label className="text-sm text-gray-700">Username</label>
+          <label className="text-sm text-gray-700">E-mail</label>
           <input
             type="email"
             name="email"
--- my-app/src/components/UpdateButton.tsx
+++ my-app/src/components/UpdateButton.tsx
@@ -1,10 +1,16 @@
 "use client";
 
+import { useFormStatus } from "react-dom";
+
 const UpdateButton = () => {
+  const { pending } = useFormStatus();
   return (
-    <div className="bg-pink-500 text-white p-2 rounded-md disabled:bg-pink-200 disabled:cursor-not-allowed max-w-96">
-      Update
-    </div>
+    <button
+      disabled={pending}
+      className="bg-pink-500 text-white p-2 cursor-pointer rounded-md disabled:bg-pink-200 disabled:cursor-not-allowed max-w-96"
+    >
+      {pending ? "Update..." : "Update"}
+    </button>
   );
 };
 
